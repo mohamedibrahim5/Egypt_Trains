d95209773fefcfbf80f48bdd53877fa164510576
mohamedibrahim5 <mohamedibrahimm355@gmail.com>
2022-08-06 19:30:35 +0200

    Adding Switch Controller And Finish All Features In First Screen

--- trains/ViewController.swift
+++ trains/ViewController.swift
@@ -9,8 +9,49 @@ import UIKit
 import DropDown
 
 class ViewController: UIViewController,UISearchBarDelegate {
+    
+    
+    
+    @IBAction func results(_ sender: UIButton) {
+        print("result")
+    }
+    @IBAction func Switch(_ sender: UIButton) {
+        print("switch")
+        var switchU : String?
+        switchU = UserDefaults.standard.string(forKey: "arr")
+        var switchUi : String?
+        switchUi = UserDefaults.standard.string(forKey: "dep")
+        UserDefaults.standard.set(switchU!, forKey: "dep")
+        UserDefaults.standard.set(switchUi!, forKey: "arr")
+        depText.text = switchU!
+        arrText.text = switchUi!
+        print(switchU!)
+        print(switchUi!)
+        
+        
+    }
+    @IBAction func stopsButton(_ sender: UIButton) {
+        dropDown4.show()
+        dropDown4.reloadAllComponents()
+        searchBar.isHidden = false
+        view2.isHidden = false
+        x = 4
+    }
+    @IBOutlet weak var stopsLabel: UILabel!
+    @IBOutlet weak var stops: UIView!
+    @IBOutlet weak var trainClassLabel: UILabel!
+    @IBAction func trainClassButton(_ sender: UIButton) {
+        dropDown3.show()
+        dropDown3.reloadAllComponents()
+        searchBar.isHidden = false
+        view2.isHidden = false
+        x = 3
+    }
+    @IBOutlet weak var traainClass: UIView!
     var dep : String?
     var arr : String?
+    var trainClass :String?
+    var stop : String?
     @IBAction func contactUs(_ sender: UIButton) {
         if UserDefaults.standard.string(forKey: "dep") == nil || UserDefaults.standard.string(forKey: "arr") == nil  {
             print("koko")
@@ -50,10 +91,16 @@ class ViewController: UIViewController,UISearchBarDelegate {
     @IBOutlet weak var depText: UILabel!
     let dropDown = DropDown()
     let dropDown2 = DropDown()
+    let dropDown3 = DropDown()
+    let dropDown4 = DropDown()
     var trains = ["belqas","sherbin","mansoura","damnhor","fakos","alexandria","egypt","sedigaber","ismailey","mahla","tanta","portsaid","portfoad","metkhames","banha","dommite","gamsa","kafrelshikeh","helwan","shobra","menof","zagzeg","ayyad","25 janunery","damnhor","cairo","elraml"]
     var trainsFilter : [String] = []
     var trains2 = ["belqas","sherbin","mansoura","damnhor","fakos","alexandria","egypt","sedigaber","ismailey","mahla","tanta","portsaid","portfoad","metkhames","banha","dommite","gamsa","kafrelshikeh","helwan","shobra","menof","zagzeg","ayyad","25 janunery","damnhor","cairo","elraml"]
     var trainsFilter2 : [String] = []
+    var trainClasses : [String] = ["All","Sleep","Vip","Air Conditioned","NoN Air Conditioned","Disticet","Improved"]
+    var trainClassFilter : [String] = []
+    var stopsTrains : [String] = ["Any","Direct","Major Cities","Express","Suburbs"]
+    var stopsTrainsFilter : [String] = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -62,6 +109,8 @@ class ViewController: UIViewController,UISearchBarDelegate {
         searchBar.delegate = self
         trainsFilter = trains
         trainsFilter2 = trains2
+        trainClassFilter = trainClasses
+        stopsTrainsFilter = stopsTrains
         if UserDefaults.standard.string(forKey: "dep") == nil {
             depText.text = "select a dep"
         } else {
@@ -71,6 +120,16 @@ class ViewController: UIViewController,UISearchBarDelegate {
             arrText.text = "select a arr"
         } else {
             arrText.text = UserDefaults.standard.string(forKey: "arr")
+        }
+        if UserDefaults.standard.string(forKey: "class") == nil {
+            trainClassLabel.text = "select a train class"
+        } else {
+            trainClassLabel.text = UserDefaults.standard.string(forKey: "class")
+        }
+        if UserDefaults.standard.string(forKey: "stops") == nil {
+            stopsLabel.text = "select a train stops"
+        } else {
+            stopsLabel.text = UserDefaults.standard.string(forKey: "stops")
         }
         
         
@@ -100,6 +159,33 @@ class ViewController: UIViewController,UISearchBarDelegate {
             arr = trainsFilter2[index]
             UserDefaults.standard.set(trainsFilter2[index], forKey: "arr")
         }
+        
+        dropDown3.anchorView = view3
+        dropDown3.dataSource = trainClassFilter
+        dropDown3.bottomOffset = CGPoint(x: 0, y: (dropDown.anchorView?.plainView.bounds.height)!)
+        dropDown3.topOffset = CGPoint(x: 0, y: -(dropDown.anchorView?.plainView.bounds.height)!)
+        dropDown3.direction = .bottom
+        dropDown3.selectionAction = { [unowned self] (index:Int , item:String) in
+            print("selected item : \(item) at index \(index)")
+            self.trainClassLabel.text = trainClassFilter[index]
+            searchBar.isHidden = true
+            view2.isHidden = true
+            trainClass = trainClassFilter[index]
+            UserDefaults.standard.set(trainClassFilter[index], forKey: "class")
+        }
+        dropDown4.anchorView = view3
+        dropDown4.dataSource = stopsTrainsFilter
+        dropDown4.bottomOffset = CGPoint(x: 0, y: (dropDown.anchorView?.plainView.bounds.height)!)
+        dropDown4.topOffset = CGPoint(x: 0, y: -(dropDown.anchorView?.plainView.bounds.height)!)
+        dropDown4.direction = .bottom
+        dropDown4.selectionAction = { [unowned self] (index:Int , item:String) in
+            print("selected item : \(item) at index \(index)")
+            self.stopsLabel.text = stopsTrainsFilter[index]
+            searchBar.isHidden = true
+            view2.isHidden = true
+            stop = stopsTrainsFilter[index]
+            UserDefaults.standard.set(stopsTrainsFilter[index], forKey: "stops")
+        }
 
         
     }
@@ -118,6 +204,20 @@ class ViewController: UIViewController,UISearchBarDelegate {
             print("searchText \(searchText)")
             dropDown2.show()
             dropDown2.dataSource = trainsFilter2
+        } else if x == 3 {
+            trainClassFilter = searchText.isEmpty ? trainClasses : trainClasses.filter({ (dat) -> Bool in
+                dat.range(of: searchText, options: .caseInsensitive) != nil
+            })
+            print("searchText \(searchText)")
+            dropDown3.show()
+            dropDown3.dataSource = trainClassFilter
+        } else if x == 4 {
+            stopsTrainsFilter = searchText.isEmpty ? stopsTrains : stopsTrains.filter({ (dat) -> Bool in
+                dat.range(of: searchText, options: .caseInsensitive) != nil
+            })
+            print("searchText \(searchText)")
+            dropDown4.show()
+            dropDown4.dataSource = stopsTrainsFilter
         }
     }
 }
